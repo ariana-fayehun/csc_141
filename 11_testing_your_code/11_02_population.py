266dc1cc136d4f65e0281251a05da5c26955da4c
omololaf <omololaf@icloud.com>
2024-11-08 11:00:26 -0500

    Finished chapter 11 assignments 1-2

---
+++ 11_testing_your_code/11_02_population.py
@@ -0,0 +1,5 @@
+# I rate this assignment a 1 because I only had to add a few things from the
+# previous assignment
+
+# The work for this assignment is in city_functions_population and 
+# test_cities_population
